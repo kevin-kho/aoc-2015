7ae6b78eea3d2075710e4953cfef929f5d0c5cc4
Kevin Ho <kevinkh08@gmail.com>
2025-12-19 10:14:07 -0800

    day18 part2 complete

--- day18/main.go
+++ day18/main.go
@@ -65,6 +65,21 @@ func getNextState(grid [][]byte) [][]byte {
 
 }
 
+func getNextStateNonCorner(grid [][]byte) [][]byte {
+	var newGrid [][]byte
+	for _, row := range grid {
+		newGrid = append(newGrid, slices.Clone(row))
+	}
+
+	for y, row := range grid {
+		for x := range row {
+			newGrid[y][x] = toggleNonCornerLight(grid, x, y)
+		}
+	}
+
+	return newGrid
+}
+
 func toggleLight(grid [][]byte, x, y int) byte {
 	onNeighbors := checkNeighbors(grid, x, y)
 
@@ -84,6 +99,34 @@ func toggleLight(grid [][]byte, x, y int) byte {
 
 }
 
+func toggleNonCornerLight(grid [][]byte, x, y int) byte {
+	onNeighbors := checkNeighbors(grid, x, y)
+	corners := map[Dir]bool{
+		{X: 0, Y: 0}:                            true,
+		{X: len(grid[0]) - 1, Y: 0}:             true,
+		{X: 0, Y: len(grid) - 1}:                true,
+		{X: len(grid[0]) - 1, Y: len(grid) - 1}: true,
+	}
+
+	if corners[Dir{X: x, Y: y}] {
+		return grid[y][x]
+	}
+
+	// case: on
+	if grid[y][x] == 35 {
+		if onNeighbors == 2 || onNeighbors == 3 {
+			return 35 // stay on
+		}
+		return 46 // turn off
+	}
+
+	// case: off
+	if onNeighbors == 3 {
+		return 35 // turn on
+	}
+	return 46
+}
+
 func countLightsOn(grid [][]byte) int {
 	var count int
 	for _, row := range grid {
@@ -96,6 +139,26 @@ func countLightsOn(grid [][]byte) int {
 	return count
 }
 
+func solvePartOne(data []byte) {
+
+	grid := getGrid(data)
+	for range 100 {
+		grid = getNextState(grid)
+	}
+	res := countLightsOn(grid)
+	fmt.Println(res)
+
+}
+
+func solvePartTwo(data []byte) {
+	grid := getGrid(data)
+	for range 100 {
+		grid = getNextStateNonCorner(grid)
+	}
+	res := countLightsOn(grid)
+	fmt.Println(res)
+}
+
 func main() {
 	filePath := "./input.txt"
 	data, err := common.ReadInput(filePath)
@@ -103,13 +166,8 @@ func main() {
 		log.Fatal(err)
 	}
 	data = common.TrimNewLineSuffix(data)
+	solvePartOne(data)
 
-	grid := getGrid(data)
-
-	for range 100 {
-		grid = getNextState(grid)
-	}
-	res := countLightsOn(grid)
-	fmt.Println(res)
+	solvePartTwo(data)
 
 }
